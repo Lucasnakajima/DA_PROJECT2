3d88b67471da11e2eabd63f493f82b15411e30c6
Lucasnakajima <nakajima.lucas02@gmail.com>
2023-05-31 09:54:20 -0400

    test

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ int main() {
     load_graphs l;
     graph_toy = l.load_toy("stadiums.csv");
     /*Graph graph_real(true);
+      int c;
     graph_real = l.load_real("graph3");*/
     Graph graph_medium(true);
     graph_medium = l.load_medium(25);
